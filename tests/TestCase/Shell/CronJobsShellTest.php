25b4d6edc1d04f19bf162ae4849fd3321abec1e1
nojimage <nojimage@gmail.com>
2019-11-10 16:04:14 +0900

    fixes test for <=3.7

--- tests/TestCase/Shell/CronJobsShellTest.php
+++ tests/TestCase/Shell/CronJobsShellTest.php
@@ -5,12 +5,8 @@
 
 namespace Elastic\CronJobs\Test\TestCase\Shell;
 
-use Cake\Console\ConsoleInput;
-use Cake\Console\ConsoleIo;
-use Cake\Console\Exception\StopException;
 use Cake\Core\Plugin;
 use Cake\TestSuite\ConsoleIntegrationTestCase;
-use Cake\TestSuite\Stub\ConsoleOutput;
 use Elastic\CronJobs\Shell\CronJobsShell;
 
 /**
@@ -18,20 +14,19 @@ use Elastic\CronJobs\Shell\CronJobsShell;
  */
 class CronJobsShellTest extends ConsoleIntegrationTestCase
 {
+	/**
+	 * ConsoleIo mock
+	 *
+	 * @var \Cake\Console\ConsoleIo|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	public $io;
 
-    /**
-     * ConsoleIo mock
-     *
-     * @var \Cake\Console\ConsoleIo|\PHPUnit_Framework_MockObject_MockObject
-     */
-    public $io;
-
-    /**
-     * Test subject
-     *
-     * @var \Elastic\CronJobs\Shell\CronJobsShell
-     */
-    public $CronJobs;
+	/**
+	 * Test subject
+	 *
+	 * @var \Elastic\CronJobs\Shell\CronJobsShell
+	 */
+	public $CronJobs;
 
 	/**
 	 * @var string
@@ -44,47 +39,47 @@ class CronJobsShellTest extends ConsoleIntegrationTestCase
 	private $testFilepath;
 
 	/**
-     * setUp method
-     *
-     * @return void
-     */
-    public function setUp()
-    {
-        parent::setUp();
-        $this->io = $this->getMockBuilder('Cake\Console\ConsoleIo')->getMock();
-        $this->CronJobs = new CronJobsShell($this->io);
+	 * setUp method
+	 *
+	 * @return void
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+		$this->io = $this->getMockBuilder('Cake\Console\ConsoleIo')->getMock();
+		$this->CronJobs = new CronJobsShell($this->io);
 
-        $this->tasksPath = Plugin::path('Elastic/CronJobs') . 'tests/Fixture/tasks/';
+		$this->tasksPath = Plugin::path('Elastic/CronJobs') . 'tests/Fixture/tasks/';
 		$this->testFilepath = Plugin::path('Elastic/CronJobs') . 'tests/tmp/crunz-time';
 		if (file_exists($this->testFilepath)) {
 			unlink($this->testFilepath);
 		}
 	}
 
-    /**
-     * tearDown method
-     *
-     * @return void
-     */
-    public function tearDown()
-    {
-        unset($this->CronJobs);
+	/**
+	 * tearDown method
+	 *
+	 * @return void
+	 */
+	public function tearDown()
+	{
+		unset($this->CronJobs);
 
-        parent::tearDown();
-    }
+		parent::tearDown();
+	}
 
-    /**
-     * Test main method
-     *
-     * @return void
-     */
-    public function testCanGetScheduleList()
-    {
-    	$this->exec(sprintf('Elastic/CronJobs.CronJobs schedule:list %s', $this->tasksPath));
+	/**
+	 * Test main method
+	 *
+	 * @return void
+	 */
+	public function testCanGetScheduleList()
+	{
+		$this->exec(sprintf('Elastic/CronJobs.CronJobs schedule:list %s', $this->tasksPath));
 
-    	$this->assertExitSuccess();
-    	// TODO: check output
-    }
+		$this->assertExitCode(0);
+		// TODO: check output
+	}
 
 	/**
 	 * Test main method
@@ -95,7 +90,7 @@ class CronJobsShellTest extends ConsoleIntegrationTestCase
 	{
 		$this->exec(sprintf('Elastic/CronJobs.CronJobs schedule:run %s', $this->tasksPath));
 
-		$this->assertExitSuccess();
+		$this->assertExitCode(0);
 		$this->assertFileExists($this->testFilepath);
 	}
 }
